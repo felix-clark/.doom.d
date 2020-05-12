a954d84dd0d44172ce55c88a497b784c5339c4cc
Felix Clark <mfclark3690@gmail.com>
2020-05-12 12:09:03 -0400

    adding untested python attach template

---
+++ attach-python.el
@@ -0,0 +1,28 @@
+;;; attach-python.el --- Attach to a remote python session
+
+;;; Commentary:
+;;; Drop this in the root of a containerized application and edit the directories below.
+;;; Open and execute it with "SPC m e b".
+
+;;; Code:
+
+;; Taken from https://donjayamanne.github.io/pythonVSCodeDocs/docs/debugging_remote-debugging/
+;; UNTESTED
+(dap-register-debug-template "Python attach remote"
+  (list :type "python"
+    :name "Python attach (remote debug)"
+    :request "attach"
+    :localRoot default-directory
+    ;; Assumes the project source is in /app in the remote container
+    :remoteRoot "/app"
+    ;; The default port for remote debugging is usually 5678
+    :port 5678
+    ;; :secret "debug auth pass phrase"
+    ;; This may need tweaking. TRAMP needs the leading /
+    :host "/docker:<CONTAINER NAME>"
+    ;; :url "http://localhost:80"
+    )
+  )
+
+(provide 'attach-python)
+;;; attach-python.el ends here
