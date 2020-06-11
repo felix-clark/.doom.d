4c7f78a67f09c1ac9bf46c50402e69a360ec08ab
Felix Clark <mfclark3690@gmail.com>
2020-06-11 15:46:07 -0400

    more org features

--- init.el
+++ init.el
@@ -146,7 +146,8 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       ;; (org +dragndrop +journal +present +roam)               ; organize your plain life in plain text
+       (org +dragndrop +journal +present)               ; organize your plain life in plain text
        ;;perl              ; write code no one else can comprehend
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
@@ -174,7 +175,7 @@
        ;;(wanderlust +gmail)
 
        :app
-       ;;calendar
+       calendar
        ;;irc               ; how neckbeards socialize
        ;;(rss +org)        ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
